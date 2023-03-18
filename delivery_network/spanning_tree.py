5fe96bd3b041f47964acfe4de8dd559468d14d0b
Fanny46 <fanny.daubet@gmail.com>
2023-03-18 12:59:34 +0100

    path_spanning_tree2

--- delivery_network/spanning_tree.py
+++ delivery_network/spanning_tree.py
@@ -1,35 +1,36 @@
-from graph import Graph, graph_from_file_route
+from graph import Graph, graph_from_file_route, graph_from_file
+from random import choice
 
 #******************** Question 12 ********************
 class UnionFind:
     def __init__ (self, G = Graph()) :
-        self.parent = {k : k for k in G.nodes} # le parent va permettre d'associer aux sommets d'un sous-ensemble un unique sommet qui sera leur "père" car on veut construire un arbre
-        self.rank = {k : 0 for k in G.nodes}
+        self.root = {k : k for k in G.nodes} # la racine va permettre d'associer aux sommets d'un sous-ensemble un unique sommet qui sera leur "racine" car on veut construire un arbre
+        self.rank = {k : 0 for k in G.nodes} # nombre de fils par sommet
 
-    def find(self, k) : # on trouve le père d'un sommet au sein d'un sous-arbre
+    def find(self, k) : # on trouve la racine d'un sommet au sein d'un sous-arbre
         """_summary_
-        On trouve le père d'un sommet au sein d'un sous-arbre.
+        On trouve la racine d'un sommet au sein d'un sous-arbre.
         Args:
-            k (int): sommet dont on veut trouver le père
+            k (int): sommet dont on veut trouver la racine
         """
-        if self.parent[k] != k :      
-            return self.find(self.parent[k])
+        if self.root[k] != k :
+            return self.find(self.root[k])
         else : return k
 
-    def union(self, x, y) : # pour construire l'arbre, on relie deux sous-arbres par leurs pères : l'un devient le père de l'autre
-        """Permet de relier deux sous-abres par leurs père (nécéssaire à la construction du graph)
+    def union(self, x, y) : # pour construire l'arbre, on relie deux sous-arbres par leur racine : l'une devient la racine de l'autre
+        """Permet de relier deux sous-arbres par leurs racines (nécessaire à la construction du graph)
         Args:
-            x : sous-abre de père x
-            y : sous-abre de père y
+            x : sous-arbre de racine x
+            y : sous-arbre de racine y
         Returns:
             Ne retourne rien
         """
         rx = self.find(x)
         ry = self.find(y)
         if self.rank[rx] > self.rank[ry]:
-            self.parent[ry] = rx
+            self.root[ry] = rx
         else :
-            self.parent[rx] = ry
+            self.root[rx] = ry
             if self.rank[rx] == self.rank[ry] :
                 self.rank[ry] += 1
         return None
@@ -46,44 +47,94 @@ def Kruskal(G) : # prend un graph G en entrée. Complexité en O(Elog(E))
     U = UnionFind(G)
     nodes = []
     X = Graph(nodes)
-    s_edges = sorted(G.edges) # on trie les arêtes par ordre de poids
+    s_edges = sorted(G.edges, key=lambda x:x[2]) # on trie les arêtes par ordre de poids
     for edge in s_edges : # pour chaque arête, si les sommets u et v ne sont pas dans le même sous-arbre, on va les relier jusqu'à avoir un arbre unique
-        power = edge[0]
-        u = edge[1]
-        v = edge[2]
-        if U.find(u) != U.find(v): # si les deux points ne sont pas dans le même sous-abre (i.e. ils n'ont pas le même père)
+        u = edge[0]
+        v = edge[1]
+        power = edge[2]
+        if U.find(u) != U.find(v): # si les deux points ne sont pas dans le même sous-abre (i.e. ils n'ont pas la même racine)
             X.add_edge(u,v,power) # on ajoute l'arête (u,v) à l'arbre couvrant
-            U.union(u,v) # on unit les sous-arbres des sommets u et v pour qu'ils aient le même "père"(on crée un arbre, sans cycle)
-    return X
+            U.union(u,v) # on unit les sous-arbres des sommets u et v pour qu'ils aient la même racine (on crée un arbre, sans cycle)
+    root = U.find(s_edges[0][0])
+    return [X, root]
 
 
 
 #******************** Question 14 ******************** 
 # Complexité en O(V)
+def aux_parcours(S, node, path, src, dest, nodes_v, powers) : #fonction auxiliaire de path_spanning_tree qui renvoie le chemin allant d'une source à une destination et la puissance minimale pour couvrir le trajet
+    if node == dest:
+        return max(powers), path
+    for i in S.graph[node] :
+        k=i[0]
+        k_power = i[1]
+        if not nodes_v[k] :
+            nodes_v[k]=True
+            powers.append(k_power)
+            return aux_parcours(S, k, path+[k], src, dest, nodes_v, powers)
+        elif i == S.graph[node][-1] and path[-1] != src : # en cas de cul-de-sac, on repart en arrière
+            nodes_v[i[0]]=True
+            powers.pop()
+            path.pop()
+            k = path[-1]
+            return aux_parcours(S, k, path, src, dest, nodes_v, powers)
+    return None
+
 def path_spanning_tree (S, src, dest) : # prend un arbre couvrant en entrée
-    """ Should return power_min, path """
+    """Should return power_min, path"""
     nodes_v = {node : False for node in S.nodes}  # dictionnaire qui permet de savoir si l'on est déjà passé par un sommet
     nodes_v [src] = True
     powers = [0]  # on crée une liste qui contient toutes les puissances, dont on gardera le max
-    def parcours(node, path) :
-        if node == dest:
-            return max(powers), path
-        for i in S.graph[node] :
-            k=i[0]
-            k_power = i[1]
-            if not nodes_v[k] :
-                nodes_v[k]=True
-                powers.append(k_power)
-                return parcours(k, path+[k])
-            elif i == S.graph[node][-1] and path[-1] != src : # en cas de cul-de-sac, on repart en arrière
-                nodes_v[i[0]]=True
-                powers.pop()
-                path.pop()
-                k = path[-1]
-                return parcours(k, path)
-        return None
+    
+    return aux_parcours(S, src, [src], src, dest, nodes_v, powers)
 
-    return parcours(src, [src])
+
+#******************** Bonus question 16 ********************
+def depth (S, root) :
+# fonction qui renvoie la profondeur (par rapport à la racine) à laquelle se situent tous les sommets d'un arbre couvrant et leur père (associé à la puissance du trajet pour y aller)
+    parent = {k : [-1, -1 ]for k in S.nodes}
+    depth = {k : 0 for k in S.nodes}
+    parent[root] = [root, 0]
+    return aux_depth(S, root, 1, parent, depth)
+
+def aux_depth(S, node, depth_node, parent, depth) : # complexité en O(V + E)
+# fonction récursive qui renvoie la profondeur (par rapport à la racine) à laquelle se situe un sommet d'un arbre couvrant et son parent (associé à la puissance du trajet jusqu'au parent)
+    for edge in S.graph[node] :
+        node_v = edge[0]
+        power = edge[1]
+        if parent[node_v] == [-1,-1] :
+            parent[node_v] = [node, power]
+            depth[node_v] = depth_node
+            aux_depth(S, node_v, depth_node+1, parent, depth)
+    return parent, depth
+
+#l'initialisation, en comptant Kruskal, a une complexité en O(Elog(E))
+def path_spanning_tree2 (X, src, dest) : # complexité sans tenir compte de l'initialisation : O(V)
+# Should return power_min, path
+    S, root = X
+    dico_parent, dico_depth = depth(S, root)
+    path_src = [src]
+    path_dest =[dest]
+    powers = [0]
+
+    p, p_power = dico_parent[src] # si la source est plus profonde que la destination, on ajoute à path_src la portion de chemin comprise entre la source et un sommet de même profondeur que la destination
+    while dico_depth[p] > dico_depth[dest] :
+        powers += [p_power]
+        path_src += [p]
+        p, p_power = dico_parent[p]
+    q, q_power = dico_parent[dest]
+    while dico_depth[q] > dico_depth[src] : # même chose si la destination est plus profonde que la source
+        powers += [q_power]
+        path_dest.insert(0, q)
+        q, q_power = dico_parent[q]
+    while p not in path_dest and q not in path_src : # dès qu'un des parents de la source est ajouté dans path_dest, les 2 chemins montant depuis src et dest se rejoignent : on a donc trouvé l'unique chemin entre src et dest
+        powers += [p_power] + [q_power]
+        path_src += [p]
+        path_dest.insert(0, q)
+        p, p_power = dico_parent[p]
+        q, q_power = dico_parent[q]
+    path = path_src + path_dest # on obtient le chemin de src vers dest en joignant les remontées respectives
+    return max(powers), path
 
 
 #******************** Question 15 ********************
@@ -97,13 +148,12 @@ def output_routes(num_fichier):
         line = f.readline().split()
         src = int(line[0])
         dest = int(line[1])
-        h.write(str(src)+" " + str(dest) + " "+ str(path_spanning_tree(kruskal, src, dest)[0]) + "\n")
+        h.write(str(path_spanning_tree(kruskal, src, dest)[0]) + "\n")
     f.close()
     h.close()
 
 output_routes(1)
-#Le code a déjà été exécuté sur le fichier routes.1.in et peut etre consulté dans le delivery_network.
-
+#Le code a déjà été exécuté sur le fichier routes.1.in et peut être consulté dans le delivery_network.
 
 
 
--- delivery_network/time_spanning_tree.py
+++ delivery_network/time_spanning_tree.py
@@ -1,5 +1,5 @@
 from graph import graph_from_file_route
-from spanning_tree import Kruskal, path_spanning_tree
+from spanning_tree import Kruskal, path_spanning_tree2
 from time import perf_counter
 from random import choice
 
@@ -7,7 +7,8 @@ for n in range (1,10) : # on va chercher le temps total qu'il faut pour trouver 
     data_path = "input/"
     file_name = "routes."+str(n)+".in"
     G = graph_from_file_route(data_path + file_name)
-    S = Kruskal(G)
+    X = Kruskal(G)
+    S = X[0]
     time=0
     list=[l for l in S.graph.keys()]
     nb_tests = 8
@@ -16,7 +17,7 @@ for n in range (1,10) : # on va chercher le temps total qu'il faut pour trouver 
         b=choice(list)
         time_begin=perf_counter()
 
-        path_spanning_tree(S, a, b)
+        path_spanning_tree2(X, a, b)
 
         time_stop=perf_counter()
         time+=time_stop-time_begin
@@ -25,15 +26,23 @@ for n in range (1,10) : # on va chercher le temps total qu'il faut pour trouver 
     
     tot_time_sec = (time/nb_tests)*S.nb_edges # à partir du temps moyen obtenu, on calcule le temps qu'il faudrait pour trouver tous les chemins possibles dans le graphe
     tot_time_min = tot_time_sec / 60
-    print("Pour calculer l'ensemble des trajets de", file_name, "il faudra : ", tot_time_sec, "secondes, soit", tot_time_min,"min.")
+    print("Pour calculer l'ensemble des trajets de", file_name, "il faudra : ", tot_time_sec, "s., soit", tot_time_min,"min., soit", tot_time_min/60, "h.")
 
 
 
 """************ Début des résultats d'exécution **************************
+Avec la fonction path_spanning_tree :
+
 Pour calculer un trajet de routes.1.in il s'est écoulé en moyenne :  7.075053872540593e-06 secondes.
 Pour calculer l'ensemble des trajets de routes.1.in il faudra :  0.00013442602357827127 secondes, soit 2.240433726304521e-06 min.
+RecursionError: maximum recursion depth exceeded in comparison
 
-RecursionError: maximum recursion depth exceeded in comparison"""
-# la fonction récursive "parcours" boucle trop sur elle-même
+Avec la fonction path_spanning_tree2 :
 
-# les algorithmes que nous avons implémentés dans le TD2 n'ont donc pas permis de réduire le temps de calcul.
+Pour calculer un trajet de routes.1.in il s'est écoulé en moyenne :  1.4624965842813253e-05 secondes.
+Pour calculer l'ensemble des trajets de routes.1.in il faudra :  0.0002778743510134518 s., soit 4.63123918355753e-06 min., soit 7.718731972595883e-08 h.
+Pour calculer un trajet de routes.2.in il s'est écoulé en moyenne :  0.27605456253513694 secondes.
+Pour calculer l'ensemble des trajets de routes.2.in il faudra :  23160.42568757292 s., soit 386.00709479288196 min., soit 6.433451579881366 h.
+Pour calculer un trajet de routes.3.in il s'est écoulé en moyenne :  0.4216748250182718 secondes.
+Pour calculer l'ensemble des trajets de routes.3.in il faudra :  42164.95245287707 s., soit 702.7492075479512 min., soit 11.712486792465853 h.
+"""
